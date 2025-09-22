d038fc1cbab6e3db1157773fe79d57d7d7e4fd3d
Chaitanya Garud <125890249+Chaitanya-Garud@users.noreply.github.com>
2025-09-22 20:20:45 +0530

    Update main.tf

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,5 +1,10 @@
+variable "subscription_id" {}
+variable "tenant_id" {}
+
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
 
 module "network" {
