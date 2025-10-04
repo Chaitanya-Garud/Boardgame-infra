140a86504682aa6611f6deb19b7bb9a236d72697
Chaitanya Garud <125890249+Chaitanya-Garud@users.noreply.github.com>
2025-10-04 14:28:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.0"
+      version = "~> 4.0"         # This uses the latest compatible azurerm v4.x provider, avoiding older versions
     }
   }
 
-  # Remote backend configuration using Azure Storage Account
   backend "azurerm" {
     resource_group_name  = "DevOps_Karma"
     storage_account_name = "devchaitanya019"
@@ -14,6 +13,7 @@ terraform {
     key                  = "production/terraform.tfstate"
   }
 }
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
